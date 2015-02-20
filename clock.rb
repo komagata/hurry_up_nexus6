4959487d6ebc3f97e64b70787e71a4107de38fb1
Masaki Komagata <komagata@gmail.com>
2015-02-20 14:26:31 +0900

    Check all colors and all models

--- clock.rb
+++ clock.rb
@@ -4,7 +4,9 @@ require 'css_selector'
 require 'clockwork'
 include Clockwork
 
-NEXUS6_URL = 'https://play.google.com/store/devices/details/Nexus_6_64_GB_%E3%82%AF%E3%83%A9%E3%82%A6%E3%83%89_%E3%83%9B%E3%83%AF%E3%82%A4%E3%83%88?id=nexus_6_white_64gb&hl=ja'
+NEXUS6_BASE_URL = 'https://play.google.com/store/devices/details?id=nexus_6_'
+COLORS = %w(white blue)
+MEMORIES = %w(32 64)
 
 Mail.defaults do
   delivery_method :smtp, {
@@ -19,17 +21,22 @@ Mail.defaults do
 end
 
 handler do |job, time|
-  html = open(NEXUS6_URL) { |f| f.read }
-  status = CssSelector.new.parse(html, '.shipping-status').strip
-  if status != '現在在庫切れです。しばらくしてからもう一度ご確認ください。'
-    Mail.deliver do
-      from     'komagata@gmail.com'
-      to       ENV['MAIL_SEND_TO']
-      subject  'Nexus6 is now available!'
-      body     "Go fast! #{NEXUS6_URL}"
+  COLORS.each do |color|
+    MEMORIES.each do |memory|
+      url = "#{NEXUS6_BASE_URL}#{color}_#{memory}gb"
+      html = open(url) { |f| f.read }
+      status = CssSelector.new.parse(html, '.shipping-status').strip
+      if status != '現在在庫切れです。しばらくしてからもう一度ご確認ください。'
+        Mail.deliver do
+          from     'komagata@gmail.com'
+          to       'komagata@gmail.com'
+          subject  "Nexus6 #{color} #{memory}MB is now available!"
+          body     "Go fast! #{url}"
+        end
+      else
+        puts "Nexus6 #{color} #{memory}GB model is not available..."
+      end
     end
-  else
-    puts 'Not available...'
   end
 end
 
